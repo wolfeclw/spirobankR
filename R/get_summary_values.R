a6e80993f33b991012e70c330aaf6e569bd02a49
Chris Wolfe <wolfeclw@icloud.com>
2022-08-31 10:03:08 +0800

    rename get_max_values()

--- R/get_summary_values.R
+++ R/get_summary_values.R
@@ -1,12 +1,15 @@
 
-#' Calculate Maximum Spirometry Values by Test
+#' Calculate Summary Spirometry Values by Test
 #'
-#' `get_max_values`   Within each test, the maximum value for each spirometric parameter (FEV1, FVC, etc.) is calculated. .
-#' Users can specify grouping variables, and table operations will be performed 'by group."
+#' `get_summary_values` calculates summary statistics for each parameter for all trials within a test.
+#' The intent of this function is to find the maximum value of each parameter as is done according to American
+#' Thoracic society criteria, but other summary statistics can be generated as well (mean, median, sd).
+#' Users can also specify grouping variables, and table operations will be performed 'by group.'
 #'
 #' @param df a data frame created by `read_spiro_sql()` or `read_spiro_pdf().`
 #' @param grp_vars character; string of column names to used to calculate maximum values by group (i.e. 'id', 'spiro_test', etc.).
 #' See \code{\link[dplyr]{dplyr::group_by()}}.
+#' @param sum_fun character; summary function to be applied.  Must be one of c("max," "mean," "sd," "median," "min").
 #' @param basic_parameters logical; if TRUE, the default, only values for the following parameters are returned:
 #' - FEV1
 #' - FVC
@@ -21,15 +24,18 @@
 #' @examples
 #'
 #' \dontrun{
-#' get_max_values(df, test_threshold = NULL, test_col = 'Spiro_Event', grp_vars = c('ID', 'spiro_test'),
+#' get_max_values(df, test_threshold = NULL, grp_vars = c('ID', 'spiro_test'), sum_fun = 'max',
 #'                basic_parameters = TRUE)
 #'}
 #'
-get_max_values <- function(df, grp_vars = NULL, sum_fun = 'max', basic_parameters = TRUE) {
+get_summary_values <- function(df, grp_vars = NULL, sum_fun = 'max', basic_parameters = TRUE) {
 
+  if (!sum_fun %in% c('min', 'median', 'mean', 'max', 'sd')) {
+    stop('`sum_fun` must be one of c("max," "mean," "sd," "median," "min").', call. = FALSE)
+  }
 
   if(is.null(grp_vars)) {
-    warning('No grouping variables have been named. Output values represent the maximum values of all observations in the data frame.',
+    warning('No grouping variables have been named. Output values represent the summary of all observations in the data frame.',
             call. = FALSE)
   } else if (!is.null(grp_vars) & (('spiro_test' %in% names(df)) & !'spiro_test' %in% grp_vars)) {
     warning('`spiro_test` is listed in the input data frame, but not included as a grouping variable.',
@@ -57,6 +63,14 @@ get_max_values <- function(df, grp_vars = NULL, sum_fun = 'max', basic_parameter
 
   d_sum$n_trials <- d_trials$n_trials
 
-  d_sum %>%
-    dplyr::relocate(n_trials, .after = dplyr::group_cols())
+  if(is.null(grp_vars)) {
+    d_sum %>%
+      dplyr::relocate(n_trials)
+  } else {
+    d_sum %>%
+      dplyr::relocate(n_trials, .after = dplyr::group_cols())
+  }
 }
+
+
+
